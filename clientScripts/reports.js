8e19e6c5a88b4bc44f3ca97babe0501b7d4e96ff
zkm2 <99581119+zkm2@users.noreply.github.com>
2026-06-18 19:03:31 -0400

    More XSS

--- clientScripts/reports.js
+++ clientScripts/reports.js
@@ -5,14 +5,36 @@
         var n = sseData.Post
         var tbl = document.querySelector("tbody");
         var row = document.createElement("tr");
-        row.innerHTML = 
-            '<td></td>' + 
-            '<td>' +
-            '<a class="post-link" data-id="' + n + '" href="/all/' + n + '#p' + n + '">>>' + n + '</a>' +
-            '<a class="hash-link" href="/all/' + n + '#p' + n + '"> #</a>' +
-            '</td>' + 
-            '<td>' + sseData.Reason + '</td>' +
-            '<td>recently!</td>';
+        
+		let postLink = document.createElement("a");
+        postLink.className = "post-link";
+        postLink.dataset.id = n;
+        postLink.href = '/all/' + n + '#p' + n;
+        postLink.textContent = '>>' + n;
+
+        let hashLink = document.createElement("a");
+        hashLink.className = "hash-link";
+        hashLink.href = '/all/' + n + '#p' + n;
+        hashLink.textContent = ' #';
+
+        for (let i = 0; i < 4; i++) {
+            let field = document.createElement("td");
+            switch (i) {
+                case 0:
+                    break;
+                case 1:
+                    field.appendChild(postLink);
+                    field.appendChild(hashLink);
+                    break;
+                case 2:
+                    field.textContent = sseData.Reason;
+                    break;
+                case 3:
+                    field.textContent = 'recently!';
+                    break;
+                }
+                row.appendChild(field);
+        }
         tbl.insertBefore(row, tbl.firstChild.nextSibling);
     }
 
